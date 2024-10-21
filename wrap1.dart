3bdc207024ceadfa4affb892ab7bc9a84dea8ab0
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2024-10-21 08:34:40 +0530

    Create wrap1.dart

---
+++ wrap1.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+
+  Widget MyContainer(String digit){
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.red,
+      child: Text(digit,style: TextStyle(
+        fontSize: 20,
+      ),),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: ()=>{},
+          child: FlutterLogo(),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Wrap Example"),
+        ),
+        body: Material(
+          child: Wrap(
+            direction: Axis.vertical,
+            spacing: 10,
+            runSpacing: 10,
+            children: [
+              MyContainer("1"),
+              MyContainer("2"),
+              MyContainer("3"),
+              MyContainer("4"),
+              MyContainer("5"),
+              MyContainer("6"),
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
