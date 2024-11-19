ee2a789a1518827d030448f33433879aa00d785c
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2024-11-19 08:33:35 +0530

    Update networking.dart

--- networking.dart
+++ networking.dart
@@ -1,11 +1,14 @@
+import 'package:app_three/singleUser.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 class NetworkingExample extends StatefulWidget {
   const NetworkingExample({super.key});
   @override
   State<NetworkingExample> createState() => _NetworkingExampleState();
 }
 class _NetworkingExampleState extends State<NetworkingExample> {
+  dynamic userData = '';
   @override
   void initState()  {
     // TODO: implement initState
@@ -16,20 +19,42 @@ class _NetworkingExampleState extends State<NetworkingExample> {
     var response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
     print("user data ");
     print(response.body);
-}
-
-
+    var jsonData = json.decode(response.body);
+    setState(() {
+      userData = jsonData;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Exmaple of Networking"),
           backgroundColor: Colors.teal, foregroundColor: Colors.white),
       body: Center(
-        child: Container(
-          height: 200,
-          width: 300,
-          color: Colors.teal,
-        ),
+        child: GridView.builder(
+          itemCount: userData?.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1 ,
+          childAspectRatio: 6/1
+        ), itemBuilder: (BuildContext context , int index){
+          return Card(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: (){
+                    Navigator.pushNamed(context, '/singleUser',arguments : userData[index]['id']
+                    );
+                  },
+                  child: ListTile(
+                    leading: Icon(Icons.people_alt_sharp),
+                    title: Text("Name:" + userData[index]['name'].toString()),
+                    subtitle: Text("Email: " + userData[index]['email'].toString()),
+                  ),
+                )
+              ],
+            ),
+          );
+          },
+         ),
       ),
     );
   }
